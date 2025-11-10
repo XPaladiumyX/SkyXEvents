42e69791f9f1335697bb50584739e38861206742
XPaladiumyX <allegra.yann@gmail.com>
2025-11-10 23:50:40 +0100

    first commit

---
+++ src/main/java/skyxnetwork/skyXEvents/SkyXEvents.java
@@ -0,0 +1,17 @@
+package skyxnetwork.skyXEvents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyXEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
